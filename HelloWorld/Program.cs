662a79265c0f8102f308e31cf1b35eed32ce5f8f
Anton Dørge Friis <74772502+AntonFriis@users.noreply.github.com>
2021-09-08 13:49:29 +0200

    Refactorbranch (#2)
    * refactors
    
    * Implemented error handling and more.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,29 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Write a year to check if it's a leap year:");
+            var year = Console.ReadLine();
+            var parsedYear = 0;
+            try
+            {
+                parsedYear = Int32.Parse(year);
+                Console.WriteLine(IsLeapYear(parsedYear) ? "yay" : "nay");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse '{year}'");
+            }
         }
+
+        public static bool IsLeapYear(int year)
+        {
+            if (year < 1582) return false;
+            if (year % 4 != 0) return false;
+            if (year % 100 == 0)
+            {
+                return year % 400 == 0;
+            }
+            return true;
+        } 
     }
 }
