1942739fffcbaadfdeb946a82d4e758e7aeb9625
AntonFriis <anlf@itu.dk>
2021-09-08 13:46:37 +0200

    Implemented error handling and more.

--- HelloWorld.Tests/UnitTest1.cs
+++ HelloWorld.Tests/UnitTest1.cs
@@ -6,20 +6,41 @@ namespace HelloWorld.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Main_prints_Hello_World()
+        [Theory]
+        [InlineData(4, false)]
+        [InlineData(100, false)]
+        [InlineData(200, false)]
+        [InlineData(400, false)]
+        [InlineData(1582, false)]
+        [InlineData(1583, false)]
+        [InlineData(1584, true)]
+        [InlineData(1800, false)]
+        [InlineData(1600, true)]
+        public void LeapYear_check(int year, bool expected)
         {
-            // Arrange
+            Assert.Equal(expected, Program.IsLeapYear(year));
+        }
+
+        [Theory]
+        [InlineData("1", "nay")]
+        [InlineData("4", "nay")]
+        [InlineData("1582", "nay")]
+        [InlineData("1583", "nay")]
+        [InlineData("1584", "yay")]
+        [InlineData("1600", "yay")]
+        [InlineData("1800", "nay")]
+        [InlineData("test","Unable to parse 'test'")]
+        public void Main_IsLeapYear_checks(string input, string output)
+        {
             var writer = new StringWriter();
-            
             Console.SetOut(writer);
+            var reader = new StringReader(input);
+            Console.SetIn(reader);
+            Program.Main(Array.Empty<string>());
+
+            var actual = writer.GetStringBuilder().ToString().Trim();
             
-            // Act
-            Program.Main(new string[0]);
-            
-            // Assert
-            var output = writer.GetStringBuilder().ToString().Trim();
-            Assert.Equal("Hello World!", output);
+            Assert.Equal(output, actual);
         }
     }
 }
